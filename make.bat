66975276e9437862433f3abc630d8b41c24d3ebb
xXSchrandXx <marvinschrand@yahoo.de>
2025-04-24 18:07:26 +0200

    update make

--- make.bat
+++ make.bat
@@ -12,6 +12,7 @@ xcopy .\node_modules\leaflet\dist\leaflet.js.map .\files\js\3rdParty\leaflet\ /y
 xcopy .\node_modules\leaflet\dist\leaflet.css .\files\js\3rdParty\leaflet\ /y
 mkdir .\files\js\3rdParty\leaflet\images
 xcopy .\node_modules\leaflet\dist\images\*.png .\files\js\3rdParty\leaflet\images /y
+xcopy .\node_modules\leaflet\LICENSE .\files\js\3rdParty\leaflet\ /y
 
 for %%i in %PACKAGE_TYPES% do (
     del .\%%i.tar
@@ -20,7 +21,7 @@ for %%i in %PACKAGE_TYPES% do (
 
 del %PACKAGE_NAME%.tar
 del %PACKAGE_NAME%.tar.gz
-7z a -ttar -mx=9 %PACKAGE_NAME%.tar .\* -x!acptemplates -x!files -x!files_wcf -x!templates -x!%PACKAGE_NAME%.tar -x!%PACKAGE_NAME%.tar.gz -x!.git -x!.gitignore -x!.gitattributes -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!.phpcs.xml -x!Readme.md -x!pictures -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!ts -x!constants.php -x!composer.json -x!composer.lock
+7z a -ttar -mx=9 %PACKAGE_NAME%.tar .\* -x!acptemplates -x!files -x!files_wcf -x!templates -x!%PACKAGE_NAME%.tar -x!%PACKAGE_NAME%.tar.gz -x!.git -x!.gitignore -x!.gitattributes -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!.phpcs.xml -x!Readme.md -x!pictures -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!ts -x!constants.php -x!composer.json -x!composer.lock -x!.editorConfig
 7z a %PACKAGE_NAME%.tar.gz %PACKAGE_NAME%.tar
 del ".\%PACKAGE_NAME%.tar"
 
